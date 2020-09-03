93986e073daad9b05e39abeb65d88e84cb817ac3
dudstud <dimon.dudin1996@gmail.com>
2020-09-03 22:56:20 +0300

    Корректная отправка uuid

--- task/app/Document.php
+++ task/app/Document.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Document extends Model
 {
-    //
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($post) {
+            $post->{$post->getKeyName()} = (string) Str::uuid();
+        });
+    }
+    public function getIncrementing()
+    {
+        return false;
+    }
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }

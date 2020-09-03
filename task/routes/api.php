6530b565847a288163d7ecc4a8bb3ec8753be1d4
dudstud <dimon.dudin1996@gmail.com>
2020-09-03 23:39:27 +0300

    Метод PUT в API

--- task/routes/api.php
+++ task/routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::get('/v1/document/{id}','DocumentController@show');
 Route::post('/v1/document','DocumentController@store');
+Route::put('/v1/document/{document}','DocumentController@update');
